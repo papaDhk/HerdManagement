659a0be78c17bfa78bdf2723ff757071b36c50df
Papa <papa@datahawk.co>
2021-02-28 01:38:53 +0100

    Update animal controller

--- src/App/Controllers/Reproduction/AnimalsController.cs
+++ src/App/Controllers/Reproduction/AnimalsController.cs
@@ -5,6 +5,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Data.DTO.Reproduction;
+using Application.Data.Messages;
+using Application.Services;
+using HerdManagement.Domain.Reproduction.Entities;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,113 +19,92 @@ namespace Web.Controllers.Reproduction
     public class AnimalsController : ControllerBase
     {
         private IAnimalRepository _animalRepository { get; set; }
+        private IReproductionService _reproductionService { get; set; }
 
-        public AnimalsController(IAnimalRepository animalRepository)
+        public AnimalsController(IAnimalRepository animalRepository, IReproductionService reproductionService)
         {
             _animalRepository = animalRepository ?? throw new ArgumentNullException(nameof(animalRepository));
+            _reproductionService = reproductionService ?? throw new ArgumentNullException(nameof(reproductionService));
         }
 
-        [HttpPost]
-        public async Task<Female> AddNewFemaleAsync(Female female)
+        [HttpPost("AddNewAnimal")]
+        public async Task<Animal> AddNewAnimalAsync(AnimalDTO animalDTO)
         {
-            return await _animalRepository.AddNewFemaleAsync(female);
+            return await _reproductionService.AddNewAnimalAsync(animalDTO);
 
         }
 
-        [HttpPut]
-        public async Task<Female> UpdateFemaleAsync(Female female)
+        [HttpPut("UpdateAnimal")]
+        public async Task<Animal> UpdateAnimalAsync(Animal animal, int motherId, int fatherId)
         {
-            return await _animalRepository.UpdateFemaleAsync(female);
+            return await _reproductionService.UpdateAnimalAsync(animal, motherId, fatherId);
         }
 
-        [HttpGet]
+        [HttpGet(Name = "GetFemalesByHerdId")]
         public  IEnumerable<Female> GetFemalesByHerdIdAsync(int herdId)
         {
             return _animalRepository.GetFemalesByHerdId(herdId);
 
         }
 
-        [HttpGet]
+        [HttpGet("GetFemaleByNumber")]
         public Female GetFemaleByNumber(int femaleNumber)
         {
             return  _animalRepository.GetFemaleByNumber(femaleNumber);
 
         }
 
-        [HttpGet]
+        [HttpGet("GetFemaleById")]
         public Female GetFemaleById(int femaleId)
         {
             return _animalRepository.GetFemaleById(femaleId);
 
         }
 
-        [HttpGet]
+        [HttpGet("GetFemaleWithReproductionsById")]
         public Female GetFemaleWithReproductionsById(int femaleId)
         {
             return _animalRepository.GetFemaleWithReproductionsById(femaleId);
 
         }
 
-        [HttpPost]
-        public async Task<Male> AddNewMaleAsync(Male male)
-        {
-            return await _animalRepository.AddNewMaleAsync(male);
-
-        }
-
-        [HttpPut]
-        public async Task<Male> UpdateMaleAsync(Male male)
-        {
-            return await _animalRepository.UpdateMaleAsync(male);
-
-        }
-
-        [HttpGet]
+        [HttpGet("GetMalesByHerdId")]
         public IEnumerable<Male> GetMalesByHerdId(int herdId)
         {
             return _animalRepository.GetMalesByHerdId(herdId);
 
         }
 
-        [HttpGet]
+        [HttpGet("GetMaleByNumber")]
         public Male GetMaleByNumber(int maleNumber)
         {
             return _animalRepository.GetMaleByNumber(maleNumber);
 
         }
 
-        [HttpGet]
+        [HttpGet("GetMaleById")]
         public Male GetMaleById(int maleId)
         {
             return  _animalRepository.GetMaleById(maleId);
 
         }
 
-        [HttpPost]
-        public async Task<YoungAnimal> AddNewYoungAnimalAsync(YoungAnimal youngAnimal)
-        {
-            return await _animalRepository.AddNewYoungAnimalAsync(youngAnimal);
-        }
-
-        [HttpPut]
-        public async Task<YoungAnimal> UpdateYoungAnimalAsync(YoungAnimal youngAnimal)
-        {
-            return await _animalRepository.UpdateYoungAnimalAsync(youngAnimal);
-        }
-
-        [HttpGet]
+        [HttpGet("GetYoungAnimalsByHerdId")]
         public IEnumerable<YoungAnimal> GetYoungAnimalsByHerdId(int herdId)
         {
             return _animalRepository.GetYoungAnimalsByHerdId(herdId);
 
         }
 
-        [HttpGet]
+        [HttpGet("GetYoungAnimalByNumber")]
         public YoungAnimal GetYoungAnimalByNumber(int youngAnimalNumber)
         {
             return _animalRepository.GetYoungAnimalByNumber(youngAnimalNumber);
-
         }
 
+        public async Task<ReproductionCreationResponse> CreateOrUpdateReproductionAsync(HerdManagement.Domain.Reproduction.Entities.Reproduction reproduction)
+        {
+            return await _reproductionService.CreateOrUpdateReproductionAsync(reproduction);
+        }
     }
 }
