732a31b2aad9ef94a6bb6ff75b1a2380c5d9c669
papaDhk <papa@datahawk.co>
2023-05-01 20:27:14 +0000

    service add

---
+++ UI/Data/SpecieBreedDataService.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+
+namespace UI.Data
+{
+    public class SpecieBreedDataService
+    {
+        private readonly HttpClient _httpClient;
+
+        public SpecieBreedDataService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        
+        
+    }
+}
