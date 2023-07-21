42bb65b65925b16449cec5ec09b6cd9ddea2593c
papaDhk <papa@datahawk.co>
2023-07-21 20:58:58 +0000

    add files for desktop

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp1;
+
+public partial class App : Microsoft.Maui.Controls.Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,67 @@
+﻿using System.Reflection;
+using Applicattion.Services;
+using HerdManagement.Domain.Characteristic.Repositories;
+using HerdManagement.Domain.Common.Repositories;
+using HerdManagement.Domain.Feeding.Repository;
+using HerdManagement.Domain.Reproduction.Repository;
+using HerdManagement.Domain.SpecieBreed.Repository;
+using HerdManagement.Infrastructure.Persistence.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using SharedComponents.Utils;
+using Syncfusion.Blazor;
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts => { fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"); });
+
+        builder.Services.AddMauiBlazorWebView();
+        
+        var a = Assembly.GetExecutingAssembly();
+        using var stream = a.GetManifestResourceStream("MauiApp1.appsettings.json");
+
+        var config = new ConfigurationBuilder()
+            .AddJsonStream(stream)
+            .Build();
+
+
+        builder.Configuration.AddConfiguration(config);
+
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
+#endif
+
+        builder.Services.AddDbContext<HerdManagementDbContext>(options =>
+                options.UseSqlServer("Server=localhost,1433;Database=HerdManagement2;User Id=sa;Password=Azerty12345;Max Pool Size=1000;", options => options.MigrationsAssembly("MauiApp1"))
+            , ServiceLifetime.Transient
+        );
+
+
+        builder.Services.AddTransient<ISpecieRepository, SpecieRepository>();
+        builder.Services.AddTransient<IBreedRepository, BreedRepository>();
+        builder.Services.AddTransient<IHerdRepository, HerdRepositoryEF>();
+        builder.Services.AddTransient<IAnimalRepository, AnimalRepository>();
+        builder.Services.AddTransient<ISpecieBreedService, SpecieBreedService>();
+        builder.Services.AddTransient<IReproductionService, ReproductionService>();
+        builder.Services.AddTransient<IReproductionRepository, ReproductionRepository>();
+        builder.Services.AddTransient<IMeasurementUnitRepository, MeasurementUnitRepositoryEf>();
+        builder.Services.AddTransient<IFoodRepository, FoodRepositoryEf>();
+        builder.Services.AddTransient<IWeighingRepository, WeighingRepository>();
+        builder.Services.AddScoped<BootstrapService, BootstrapService>();
+        builder.Services.AddTransient<ICharacteristicRepository, CharacteristicRepository>();
+        builder.Services.AddTransient<IAnimalFeedingRepository, AnimalFeedingRepository>();
+        builder.Services.AddTransient<IFoodRepository, FoodRepositoryEf>();
+        builder.Services.AddSyncfusionBlazor();
+
+        return builder.Build();
+    }
+}
